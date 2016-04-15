6aee1666a162f806173e404ded2d62288ce3f47b
Leandro1391 <leandrojulian1391@gmail.com>
2016-04-15 14:59:29 -0300

    Se realizó la lógida de AdivinaElNumero1 js

--- jsAdivinaElNumero1.js
+++ jsAdivinaElNumero1.js
@@ -8,19 +8,44 @@ secreto del 1 al 100, en la pantalla del juego
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
 var numeroSecreto; 
-var contadorIntentos;
+var contadorIntentos=0;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
+	numeroSecreto=Math.floor(Math.random() * 100)+1;
+	
 	 
-		//alert(numeroSecreto );
+		alert(numeroSecreto );
 	
 
 }
 
 function verificar()
 {
-	
+	contadorIntentos++;
+	document.getElementById("intentos").value = contadorIntentos;
+	var numeroIngresado = parseInt(document.getElementById("numero").value); //obtengo el numero que ingrese en el formulario ingreso
+	//y el mismo tipo de objeto int
+
+	//Inspeccion
+	//alert("numero ingresado: " + numeroIngresado); 
+
+
+	//si es equal value and equal type ===
+
+	if(numeroIngresado == numeroSecreto)
+	{
+			alert("Usted es un ganador!!! y en solo "+contadorIntentos+" intentos");
+			document.location.href="AdivinaElNumero1.html";
+	} 
+			
+
+	if(numeroIngresado < numeroSecreto)
+			alert("falta....");
+
+	if(numeroIngresado > numeroSecreto)
+			alert("se pasó....");
+
 	
 }
